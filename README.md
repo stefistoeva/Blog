b42a7d729b7bb3e5db360efb243daaa4ad091483
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-07-16 12:05:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on July 10, 2019, 7:07 pm.
---
+++ src/SoftUniBlogBundle/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use SoftUniBlogBundle\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+     * @param Request $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        return $this->render('home/index.html.twig',
+            ['articles' => $articles]);
+    }
+}
